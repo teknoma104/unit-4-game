96ee6920b77b6239663c234f9d254583bca77464
Tommy Le <teknoma104@gmail.com>
2018-06-26 23:13:12 -0500

    Fixed character select on click function. Now produces values.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,64 +8,55 @@ const TMNTtheme = new Audio("./assets/sounds/tmnt_theme.mp3");
 $('#start-button').click(e => TMNTtheme.play());
 
 
-$(document).ready(function () {
-    $(".main-screen").html(startGameText);
-    
-});
-
-
-
-
-
 function charSelect() {
-    
-
-    $("<div></div>").addClass("row char-select").appendTo(".main-screen");
+    // $("<div></div>").addClass("row char-select").appendTo(".main-screen");
+    $(".info-text").html("Choose your turtle!");
 
     for (var x = 1; x < 5; x++) {
         var turtlePortrait = $("<img>");
         turtlePortrait.attr("width", "100px");
         turtlePortrait.attr("class", "turtle-portrait");
         console.log("Creating div col for ID char-slot-" + x);
-        $(".char-select").append('<div id="char-slot-' + x + '" class="col-md-3 text-center"></div>');
+        $(".char-select").append('<div id="char-slot-' + x + '" class="col-md-3 text-center portrait-div"></div>');
         $("#char-slot-" + x).html(turtlePortrait.attr("src", "./assets/images/portrait-" + x +".png"));
     }
 
 
 }
 
-// $(".turtle-portrait").hover(function () {
-//     $(this).css("border", "5px solid red");
-// });
-
 
 $("#start-button").click(function () {
     alert("Player clicked start the game!");
     playState = 'y';
     console.log("playState is now: " + playState);
-    $(".main-screen").html("");
+    $(".info-text").html("");
     charSelect();
 
 });
 
 
-$(".turtle-portrait").click(function () {
-    var test = $('.turtle-portrait').attr('id');
+$(document).ready(function () {
+    $(".info-text").html(startGameText); 
+});
+
+$(".char-select").on ("click", "div", function () {
+    var test = $(this).attr("id");
+    console.log("test var is assigned value:  " + test);
     console.log("Something got clicked.");
     switch (test) {
-        case 1:
+        case "char-slot-1":
             console.log("Player clicked on Donatello portrait.");
             turtleSelected = "Donatello";
             break;
-        case 2:
+        case "char-slot-2":
             console.log("Player clicked on Leonardo portrait.");
             turtleSelected = "Leonardo";
             break;
-        case 3:
+        case "char-slot-3":
             console.log("Player clicked on Michelangelo portrait.");
             turtleSelected = "Michelangelo";
             break;
-        case 4:
+        case "char-slot-4":
             console.log("Player clicked on Raphael portrait.");
             turtleSelected = "Raphael";
             break;            
