1ff532bffda60e0cc1e3187b15da593ed326e4d4
Tommy Le <teknoma104@gmail.com>
2018-06-27 09:30:35 -0500

    Character select done in game.js. Now focused on setting stage and main battle component.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,10 +18,16 @@ $(document).ready(function () {
 
 
 function charSelect() {
+<<<<<<< Updated upstream
     
 
     $("<div></div>").addClass("row char-select").appendTo(".main-screen");
+=======
+    $(".info-text").html("<p>Choose your turtle!</p>");
+>>>>>>> Stashed changes
 
+    // for loop to dynamically generate 4 new divs to store the portrait pictures of each turtle into
+    // the char select row in the main HTML
     for (var x = 1; x < 5; x++) {
         var turtlePortrait = $("<img>");
         turtlePortrait.attr("width", "100px");
@@ -30,6 +36,7 @@ function charSelect() {
         $(".char-select").append('<div id="char-slot-' + x + '" class="col-md-3 text-center"></div>');
         $("#char-slot-" + x).html(turtlePortrait.attr("src", "./assets/images/portrait-" + x +".png"));
     }
+<<<<<<< Updated upstream
 
 
 }
@@ -37,8 +44,29 @@ function charSelect() {
 // $(".turtle-portrait").hover(function () {
 //     $(this).css("border", "5px solid red");
 // });
+=======
+}
 
+function setStage() {
+    // Destroys the previous 4 portrait divs uses for selecting turtle character
+    $(".char-select").empty();
 
+    // Sets class attribute back to default so that the bio-slot isn't stuck showing a turtle's bio pic
+    $("#bio-slot").attr("class","col-lg-6 text-center");
+
+    // Sets the main screen div to stage style in CSS that has the Sewer pic as a background image
+    $(".main-screen").addClass("stage");
+
+    // Generates 4 new divs with different ID names so the char-select onclick won't trigger
+    for (var x = 1; x < 5; x++) {
+        $(".char-select").append('<div id="fighter-slot-' + x + '" class="col-md-3 text-center fighter-portrait-div"></div>');
+    }
+
+}
+>>>>>>> Stashed changes
+
+// Click event for start button to move to the next phase,
+// which is the character selection process
 $("#start-button").click(function () {
     alert("Player clicked start the game!");
     playState = 'y';
@@ -49,8 +77,22 @@ $("#start-button").click(function () {
 });
 
 
+<<<<<<< Updated upstream
 $(".turtle-portrait").click(function () {
     var test = $('.turtle-portrait').attr('id');
+=======
+$(document).ready(function () {
+    $(".info-text").html(startGameText);
+});
+
+
+// Click function used to determind which turtle the player is choosing
+// Checks the parent div ID of the image that the user clicked on to 
+// determind which turtle it is
+$(".char-select").on("click", "div", function () {
+    var test = $(this).attr("id");
+    console.log("test var is assigned value:  " + test);
+>>>>>>> Stashed changes
     console.log("Something got clicked.");
     switch (test) {
         case 1:
@@ -68,7 +110,61 @@ $(".turtle-portrait").click(function () {
         case 4:
             console.log("Player clicked on Raphael portrait.");
             turtleSelected = "Raphael";
+<<<<<<< Updated upstream
             break;            
     } 
     alert("You have selected " + turtleSelected);
 });
+=======
+            break;
+    }
+
+    // Mouseenter/mouseleave event that shows Donatello's bio in bio-slot once a user
+    // moves the mouse into Donatello's div area
+    $("#char-slot-1")
+        .mouseenter(function () {
+            $("#bio-slot").addClass("donny-bio");
+        })
+        .mouseleave(function () {
+            $("#bio-slot").removeClass("donny-bio");
+        });
+
+    // Mouseenter/mouseleave event that shows Leonardo's bio in bio-slot once a user
+    // moves the mouse into Leonardo's div area
+    $("#char-slot-2")
+        .mouseenter(function () {
+            $("#bio-slot").addClass("leo-bio");
+        })
+        .mouseleave(function () {
+            $("#bio-slot").removeClass("leo-bio");
+        });
+
+    // Mouseenter/mouseleave event that shows Michelangelo's bio in bio-slot once a user
+    // moves the mouse into Michelangelo's div area
+    $("#char-slot-3")
+        .mouseenter(function () {
+            $("#bio-slot").addClass("mikey-bio");
+        })
+        .mouseleave(function () {
+            $("#bio-slot").removeClass("mikey-bio");
+        });
+
+    // Mouseenter/mouseleave event that shows Raphael's bio in bio-slot once a user
+    // moves the mouse into Raphael's div area
+    $("#char-slot-4")
+        .mouseenter(function () {
+            $("#bio-slot").addClass("raph-bio");
+        })
+        .mouseleave(function () {
+            $("#bio-slot").removeClass("raph-bio");
+        });
+
+    // Confirms with user that the turtle the user clicked on is the one they want to use
+    // if true, calls the setStage function, if user cancels, they can still click on other turtles
+    var playerChoice = confirm("You have selected " + turtleSelected + ". Is this your final choice?");
+    if (playerChoice === true) {
+        console.log("Player has decided on " + turtleSelected);
+        setStage();
+    }
+});
+>>>>>>> Stashed changes
