4b4789e888cb3fdbd1b8307a176265bb721d2819
Tommy Le <teknoma104@gmail.com>
2018-06-28 07:09:25 -0500

    More stage building finished.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,21 @@
+var turtleDB = [
+    { n: "Donatello", hp: 100, mp: 50, atkpwr: 8, spatk: 80, spcost: 40, p: "./assets/images/don-battle-portrait.png"},
+    { n: "Leonardo", hp: 130, mp: 50, atkpwr: 9, spatk: 100, spcost: 50, p: "./assets/images/leo-battle-portrait.png"},
+    { n: "Michelangelo", hp: 120, mp: 60, atkpwr: 6, spatk: 30, spcost: 20,  p: "./assets/images/mikey-battle-portrait.png"},
+    { n: "Raphael", hp: 150, mp: 50, atkpwr: 7, spatk: 50, spcost: 25, p: "./assets/images/raph-battle-portrait.png"},
+];
+
 var playState = 'n';
 
 var startGameText = "<p>Welcome to the TMNT game! Pick a turtle and save April O' Neil from the bad guys!</p>";
 
 var turtleSelected = "";
+
+var playerHP = 0;
+var playerATK = 0;
+var playerMP = 0;
+
+var enemyHP = 0;
 
 const TMNTtheme = new Audio("./assets/sounds/tmnt_theme.mp3");
 $('#start-button').click(e => TMNTtheme.play());
@@ -39,17 +52,63 @@ function setStage() {
         $(".char-select").append('<div id="fighter-slot-' + x + '" class="col-md-3 text-center fighter-portrait-div"></div>');
     }
 
+    var battlePortrait = $("<img>");
+
+    switch (turtleSelected) {
+        case "Donatello":
+            console.log("Adding Donatello's battle portrait.");
+            playerHP = turtleDB[0].hp;
+            playerMP = turtleDB[0].mp;
+            playerATK = turtleDB[0].atkpwr;
+            battlePortrait.attr("src", turtleDB[0].p);
+            $("#spatk").html('SP ATK: ' + turtleDB[0].spatk);
+            break;
+        case "Leonardo":
+            console.log("Adding Leonardo's battle portrait.");
+            playerHP = turtleDB[1].hp;
+            playerMP = turtleDB[1].mp;
+            playerATK = turtleDB[1].atkpwr;
+            battlePortrait.attr("src", turtleDB[1].p);
+            $("#spatk").html('SP ATK: ' + turtleDB[1].spatk);
+            break;
+        case "Michelangelo":
+            console.log("Adding Michelangelo's battle portrait.");
+            playerHP = turtleDB[2].hp;
+            playerMP = turtleDB[2].mp;
+            playerATK = turtleDB[2].atkpwr;
+            battlePortrait.attr("src", turtleDB[2].p);
+            $("#spatk").html('SP ATK: ' + turtleDB[2].spatk);
+            break;
+        case "Raphael":
+            console.log("Adding Raphael's battle portrait.");
+            playerHP = turtleDB[3].hp;
+            playerMP = turtleDB[3].mp;
+            playerATK = turtleDB[3].atkpwr;
+            battlePortrait.attr("src", turtleDB[3].p);
+            $("#spatk").html('SP ATK: ' + turtleDB[3].spatk);
+            break;
+    }
+
+    $("#HP").html('HP: ' + playerHP);
+    $("#MP").html('MP: ' + playerMP);
+    $("#atk").html('ATK: ' + playerATK);
+    // $("#hero-battle-portrait").css("border","2px solid #00FF00");
+    $("#hero-battle-portrait").prepend(battlePortrait);
+    $(".initiallyHiddenBlock").show();
+    $("#atk-button").removeClass("initiallyHidden");
+    $("#spatk-button").removeClass("initiallyHidden");
+    
 }
 
 // Click event for start button to move to the next phase,
 // which is the character selection process
 $("#start-button").click(function () {
     alert("Player clicked start the game!");
+    $(this).hide();
     playState = 'y';
     console.log("playState is now: " + playState);
     $(".info-text").html("");
     charSelect();
-
 });
 
 
